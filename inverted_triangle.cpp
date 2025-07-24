96f5f6cf4543f73fa6910d335b8ea77e89ce01de
Vaishali Vala <vaishali123@gmail.com>
2025-07-24 21:20:09 +0530

    First Commit

---
+++ inverted_triangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Inverted Triangle Pattern
+
+    // * * * * *
+    // * * * *  
+    // * * *
+    // * *
+    // *
+
+    for(int i=5; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout << " * " ;
+        }cout << endl ;
+    }
+
+    return 0;
+}
