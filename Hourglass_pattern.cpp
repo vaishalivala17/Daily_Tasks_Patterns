5122031e36124aa1e8c289a848911fa33980ce79
Vaishali Vala <vaishali123@gmail.com>
2025-07-24 23:22:03 +0530

    Saving all current changes

---
+++ Hourglass_pattern.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// 	echo "# Daily_tasks_Pattern-" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/vaishalivala17/Daily_tasks_Pattern-.git
+// git push -u origin main
+
+		// *                                      * 
+		// *  *                                *  * 
+		// *  *  *                          *  *  * 
+		// *  *  *  *                    *  *  *  * 
+		// *  *  *  *  *              *  *  *  *  * 
+		// *  *  *  *  *  *        *  *  *  *  *  * 
+		// *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+
+	// for(int i=1; i<=7; i++){
+	// 	for(int j=1; j<=i; j++){
+	// 		cout << " * " ;
+	// 	}
+	// 	for(int j=7; j>i; j--){
+	// 		cout << "   " ;
+	// 	}
+	// 	for(int j=7; j>i; j--){
+	// 		cout << "   " ;
+	// 	}
+	// 	for(int j=1; j<=i; j++){
+	// 		cout << " * " ;
+	// 	}
+	// 	cout << endl ;
+	// }
+	// for(int i=6; i<1; i++){
+	// 	for(int j=7; j>=i; j--){
+	// 		cout << " * " ;
+	// 	}
+	// 	for(int j=1; j<i; j++){
+	// 		cout << "  " ;
+	// 	}
+	// 	for(int j=1; j<i; j++){
+	// 		cout << "    " ;
+	// 	}
+	// 	for(int j=7; j>=i; j--){
+	// 		cout << " * " ;
+	// 	}
+	// 	cout << endl ;
+	// }
+
+
+	// * * * * *
+	//  * * * *
+	//   * * *
+	//    * *
+	//     *
+	//    * *
+	//   * * *
+	//  * * * *
+	// * * * * *
+
+
+
+
+
+
+	
+	return 0;
+}
